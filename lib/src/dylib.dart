668ddd9811d4c6d965e2542ef303192b6c0ee24f
Paul Kepinski <paul.kepinski@orange.fr>
2024-09-29 21:55:44 +0200

    Update descriptions

--- lib/src/dylib.dart
+++ lib/src/dylib.dart
@@ -2,10 +2,7 @@ import 'dart:ffi' as ffi;
 
 import 'package:libsane/src/bindings.g.dart';
 
-// TODO: use dylib.dart
-
 LibSane? _dylib;
 LibSane get dylib {
-  return _dylib ??=
-      LibSane(ffi.DynamicLibrary.open("/usr/lib/x86_64-linux-gnu/libsane.so"));
+  return _dylib ??= LibSane(ffi.DynamicLibrary.open("libsane.so"));
 }
